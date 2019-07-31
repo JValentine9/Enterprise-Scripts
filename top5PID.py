005000d3587cfb584d495686596124f0cfc0af0f
Julian <phantomrequiem90@gmail.com>
2019-07-31 15:00:21 -0600

    Update top5PID.py
    Fixed carriage returns, and for loop.

--- top5PID.py
+++ top5PID.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='Get error and warning thresholds')
 parser.add_argument("-w","--warning",help="Percent total cpu host utilization above which a warning is generated (i.e. 90)",type=float)
 parser.add_argument("-c","--critical",help="Percent total cpu host utilization above which a critical error is generated(i.e. 100)",type=float)
 
-args = parser.parse_args()
+#args = parser.parse_args()
 
 def getListOfProcessSortedByMemory():
     '''
@@ -25,26 +25,24 @@ def getListOfProcessSortedByMemory():
            pass
 
     # Sort list of dict by key vms i.e. memory usage
-    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
-
+    listOfProcObjects = sorted(listOfProcObjects,key=lambda procObj: procObj['vms'],reverse=True)
     return listOfProcObjects
-	
-pid_list = getListOfProcessSortedByMemory[:5]
 
-for dItem in pid_list:
-	for item in dItem:
-		print(item)
+pid_list = getListOfProcessSortedByMemory
 
-##Compares system CPU times elapsed before and after interval. Should be at least .1
-#CPU_INTERVAL = 1
-#
-#cpu_pct = psutil.cpu_percent(interval=CPU_INTERVAL)
-#if cpu_pct >= args.warning and cpu_pct < args.critical:
-#        print ('WARNING: CPU utilization at {} percent.'.format(cpu_pct))
-#        sys.exit(1)
-#elif cpu_pct >= args.critical:
-#        print ('CRITICAL: CPU utilization at {} percent.'.format(cpu_pct))
-#        sys.exit(2)
-#else:
-#        print ('OK')
-#        sys.exit(0)
+for i in range(5):
+	print(pid_list)
+
+#Compares system CPU times elapsed before and after interval. Should be at least .1
+CPU_INTERVAL = 1
+
+cpu_pct = psutil.cpu_percent(interval=CPU_INTERVAL)
+if cpu_pct >= args.warning and cpu_pct < args.critical:
+        print ('WARNING: CPU utilization at {} percent.'.format(cpu_pct))
+        sys.exit(1)
+elif cpu_pct >= args.critical:
+	print ('CRITICAL: CPU utilization at {} percent.'.format(cpu_pct))
+    sys.exit(2)
+else:
+    print ('OK')
+    sys.exit(0)
