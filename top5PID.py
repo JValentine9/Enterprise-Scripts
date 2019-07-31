5fee89bfcb2c65de86b5e4d0d40f86913326f00f
Julian <phantomrequiem90@gmail.com>
2019-07-31 15:28:16 -0600

    Update top5PID.py
    commented out lines 40+ due to type diffrences.

--- top5PID.py
+++ top5PID.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='Get error and warning thresholds')
 parser.add_argument("-w","--warning",help="Percent total cpu host utilization above which a warning is generated (i.e. 90)",type=float)
 parser.add_argument("-c","--critical",help="Percent total cpu host utilization above which a critical error is generated(i.e. 100)",type=float)
 
-#args = parser.parse_args()
+args = parser.parse_args()
 
 def getListOfProcessSortedByMemory():
     '''
@@ -20,7 +20,7 @@ def getListOfProcessSortedByMemory():
            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
            pinfo['vms'] = proc.cpu_percent()
            # Append dict to list
-           listOfProcObjects.append(pinfo);
+           listOfProcObjects.append(pinfo)
        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
            pass
 
@@ -34,15 +34,16 @@ for i in range(5):
 	print(pid_list)
 
 #Compares system CPU times elapsed before and after interval. Should be at least .1
-CPU_INTERVAL = 1
+""" CPU_INTERVAL = 1
 
 cpu_pct = psutil.cpu_percent(interval=CPU_INTERVAL)
+
 if cpu_pct >= args.warning and cpu_pct < args.critical:
         print ('WARNING: CPU utilization at {} percent.'.format(cpu_pct))
         sys.exit(1)
 elif cpu_pct >= args.critical:
-	print ('CRITICAL: CPU utilization at {} percent.'.format(cpu_pct))
-    sys.exit(2)
+        print ('CRITICAL: CPU utilization at {} percent.'.format(cpu_pct))
+        sys.exit(2)
 else:
-    print ('OK')
-    sys.exit(0)
+        print ('OK')
+        sys.exit(0) """
