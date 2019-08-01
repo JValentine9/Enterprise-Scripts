f3bd7ba763794f29d4b37f39bcca935b969e1c26
heavyodin53 <39357521+heavyodin53@users.noreply.github.com>
2019-08-01 13:44:02 -0600

    Added Disk Check

---
+++ check_disk_all.py
@@ -0,0 +1,41 @@
+import psutil
+import argparse
+import sys
+import re
+
+parser = argparse.ArgumentParser(description='Get error and warning thresholds')
+parser.add_argument("-w","--warning",help="Total percent of disk space free below which will generate a warning",type=float)
+parser.add_argument("-c","--critical",help="Total percent of disk space free below which will generate a critical error",type=float)
+
+args = parser.parse_args()
+
+crits = []
+warns = []
+
+disks = psutil.disk_partitions(all=False)
+
+for disk in disks:
+    ignoreDiskTypes = re.search('cdrom', disk.opts)
+    ignoreMountPoints = re.search('(/var/lib/docker/containers|/snap.*)', disk.mountpoint)
+    if not ignoreDiskTypes and not ignoreMountPoints:
+        disk_usage = psutil.disk_usage(disk.mountpoint)
+        disk_free_pct = 100 - disk_usage.percent
+        if disk_free_pct <= args.warning and disk_free_pct > args.critical:
+                warns.append("WARNING: Device {} at mount point {} has {}pct free space!".format(disk.device, disk.mountpoint, disk_free_pct))
+        elif disk_free_pct <= args.critical:
+                crits.append("CRITICAL: Device {} at mount point {} has {}pct free space!".format(disk.device, disk.mountpoint, disk_free_pct))
+if crits and warns:
+        for x in zip(crits,warns):
+                print (x)
+        sys.exit(2)
+elif crits and not warns:
+        print (crits)
+        sys.exit(2)
+elif warns and not crits:
+        print (warns)
+        sys.exit(1)
+else:
+        print ("OK")
+        sys.exit(0)
+
+print (disks)
