0923036e15c8e0fac68bcb0967958201a46db213
Julian <phantomrequiem90@gmail.com>
2019-08-01 15:53:03 -0600

    Update top5PID.py
    Implemented a setSeverity function which runs during cpu utilization check and adds it to the dictionary. CPU Percent check now checks per process rather than as the whole system.
    Now prints PID, Name, CPU Utilization, and Severity. Skips over the first element due to it being the system idle process.

--- top5PID.py
+++ top5PID.py
@@ -9,44 +9,59 @@ parser.add_argument("-c","--critical",help="Percent total cpu host utilization a
 args = parser.parse_args()
 
 #Compares system CPU times elapsed before and after interval. Should be at least .1
-CPU_INTERVAL = 1
 
 
-def getListOfProcessSortedByMemory():
+def setSeverity(cpu_percent):
+        '''
+        Compares system CPU times elapsed before and after interval. Should be at least .1
+        '''
+        cpu_pct = cpu_percent
+
+
+        if cpu_pct >= args.warning and cpu_pct < args.critical:
+                return 'WARNING: CPU utilization at {} percent.'.format(cpu_pct)
+                #sys.exit(1)
+        elif cpu_pct >= args.critical:
+                return 'CRITICAL: CPU utilization at {} percent.'.format(cpu_pct)
+                #sys.exit(2)
+        else:
+                return 'OK'
+                #sys.exit(0)
+
+def getListOfProcessSortedByCPU():
     '''
-    Get list of running process sorted by Memory Usage
+    Get list of running process sorted by CPU Usage
     '''
+    CPU_INTERVAL = 1
     listOfProcObjects = []
+
     # Iterate over the list
     for proc in psutil.process_iter():
        try:
            # Fetch process details as dict
            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-           pinfo['vms'] = proc.cpu_percent()
+           
+           # Fetches PID and gets cpu percent by that PID
+           pID = psutil.Process(pid=pinfo["pid"])
+           pinfo['cpu'] = pID.cpu_percent(interval=CPU_INTERVAL)
+           pinfo["severity"] = setSeverity(pinfo['cpu'])
+
            # Append dict to list
            listOfProcObjects.append(pinfo)
        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
            pass
 
-    # Sort list of dict by key vms i.e. memory usage
-    listOfProcObjects = sorted(listOfProcObjects,key=lambda procObj: procObj['pid'],reverse=True)
-    return listOfProcObjects
+    # Sort list of dict by key cpu
+    listOfProcObjectsbyCPU = sorted(listOfProcObjects,key=lambda procObj: procObj['cpu'],reverse=True)
 
-pid_list = getListOfProcessSortedByMemory()[:5]
+    return listOfProcObjectsbyCPU
 
-for i in range(5):
-	print(pid_list)
+# Sets pid_list to list of dictionary and limits it to the first five dictionaries (top five)
+pid_list = getListOfProcessSortedByCPU()[1:6]
 
-#Compares system CPU times elapsed before and after interval. Should be at least .1
+# Prints "headers" 
+print("PID", "Name","CPU", "Severity")
 
-cpu_pct = psutil.cpu_percent(interval=CPU_INTERVAL)
-
-if cpu_pct >= args.warning and cpu_pct < args.critical:
-        print ('WARNING: CPU utilization at {} percent.'.format(cpu_pct))
-        sys.exit(1)
-elif cpu_pct >= args.critical:
-        print ('CRITICAL: CPU utilization at {} percent.'.format(cpu_pct))
-        sys.exit(2)
-else:
-        print ('OK')
-        sys.exit(0)
+# Prints PID, CPU and Name
+for x in pid_list:
+        print(x["pid"], x["name"], x["cpu"], x["severity"])
