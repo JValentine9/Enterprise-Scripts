0ef8e62047d7911148b38f575ded020fb83e9815
Julian <phantomrequiem90@gmail.com>
2019-08-09 15:43:52 -0600

    Optimized script and increased pagerduty api output
    Used multiprocecssing while getting CPU usage per process. Distribute the load per processor. Prints only processes that are actively using CPU.

--- top5PID.py
+++ top5PID.py
@@ -1,17 +1,19 @@
 import psutil
 import argparse
-import sys
+import sys, time, os, datetime, socket
+import multiprocessing as mp
 
 parser = argparse.ArgumentParser(description='Get error and warning thresholds')
 parser.add_argument("-w","--warning",help="Percent total cpu host utilization above which a warning is generated (i.e. 90)",type=float)
 parser.add_argument("-c","--critical",help="Percent total cpu host utilization above which a critical error is generated(i.e. 100)",type=float)
 
 args = parser.parse_args()
+now = datetime.datetime.now()
 
 #Compares system CPU times elapsed before and after interval. Should be at least .1
 
 
-def setSeverity(cpu_percent):
+def setSeverity(cpu_percent, pname):
         '''
         Compares system CPU times elapsed before and after interval. Should be at least .1
         '''
@@ -19,49 +21,56 @@ def setSeverity(cpu_percent):
 
 
         if cpu_pct >= args.warning and cpu_pct < args.critical:
-                return 'WARNING: CPU utilization at {} percent.'.format(cpu_pct)
+                return 'WARNING', 'CPU utilization at %s percent on %s'%(cpu_pct, pname)
                 #sys.exit(1)
         elif cpu_pct >= args.critical:
-                return 'CRITICAL: CPU utilization at {} percent.'.format(cpu_pct)
+                return 'CRITICAL', 'CPU utilization at %s percent on %s'%(cpu_pct, pname)
                 #sys.exit(2)
         else:
-                return 'OK'
+                return 'OK', 'CPU utilization at %s percent on %s'%(cpu_pct, pname)
                 #sys.exit(0)
 
-def getListOfProcessSortedByCPU():
-    '''
-    Get list of running process sorted by CPU Usage
-    '''
-    CPU_INTERVAL = 1
-    listOfProcObjects = []
+#proc = collections.OrderedDict()
+def GetProcessInformation(p):
+        if(p == 0): return
+        CPU_INTERVAL = 1
+        proc = {}#collections.OrderedDict()
+        try:
+                p = psutil.Process(p)
+                # Sets the proc to a dictionary with the Process information
+                # pid, name, & username are attributes that are attached to a process
+                proc['PID'], proc['Component'], proc['User'] = p.pid, p.name(), p.username()
 
-    # Iterate over the list
-    for proc in psutil.process_iter():
-       try:
-           # Fetch process details as dict
-           pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-           
-           # Fetches PID and gets cpu percent by that PID
-           pID = psutil.Process(pid=pinfo["pid"])
-           pinfo['cpu'] = pID.cpu_percent(interval=CPU_INTERVAL)
-           pinfo["severity"] = setSeverity(pinfo['cpu'])
+                # Creates a reference point for the process's CPU checks
+                #p.cpu_percent(interval=CPU_INTERVAL)
 
-           # Append dict to list
-           listOfProcObjects.append(pinfo)
-       except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-           pass
+                # Does the second CPU check and records it to the dictionary
+                proc['CPU_Usage'] = p.cpu_percent(interval=CPU_INTERVAL)
+                
+                # Checks and records the severity and summary with the setSeverity()
+                proc['Severity'], proc['Summary'] = setSeverity(proc['CPU_Usage'], proc['Component'])
+                proc['Timestamp'] = now.strftime('%Y-%m-%d %H:%M')
+                proc['Source'] = socket.gethostname() + ' - ' + socket.gethostbyname(socket.gethostname())
+                proc['Class'] = 'Process by CPU'
+                proc['Group'] = ''
+                        
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        if (bool(proc) and proc['CPU_Usage'] > 0.0):
+                return proc
 
-    # Sort list of dict by key cpu
-    listOfProcObjectsbyCPU = sorted(listOfProcObjects,key=lambda procObj: procObj['cpu'],reverse=True)
-
-    return listOfProcObjectsbyCPU
 
 # Sets pid_list to list of dictionary and limits it to the first five dictionaries (top five)
-pid_list = getListOfProcessSortedByCPU()[1:6]
+stime = time.time()
 
-# Prints "headers" 
-print("PID", "Name","CPU", "Severity")
+if __name__ == "__main__":
+        pool = mp.Pool(mp.cpu_count())
+        pid_list = list(filter(None, pool.map(GetProcessInformation, psutil.pids())))
+        
+        pid_list = sorted(pid_list, key = lambda i: i['CPU_Usage'],reverse=True)
 
-# Prints PID, CPU and Name
-for x in pid_list:
-        print(x["pid"], x["name"], x["cpu"], x["severity"])
+        print(time.time() - stime)
+
+        for x in pid_list[5]:
+                print(x['Summary'], x['Source'], x['Severity'], x['Timestamp'], x['Class'], x['Component'], x['Group'], sep='\t')
+        print()
