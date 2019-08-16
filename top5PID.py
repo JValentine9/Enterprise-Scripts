b98b6a797db43aeee4c9e3bd41f77ade659c1d08
Julian <phantomrequiem90@gmail.com>
2019-08-16 14:55:19 -0600

    Changed cpu check methods
    Used a different cpu check method to run the cpu percent passing in None adding a delay to make a more accurate check.

--- top5PID.py
+++ top5PID.py
@@ -18,7 +18,7 @@ args = args.parse_args()
 
 def influxOutput(proc):
         now = datetime.now()
-        metric = Metric("PID_CPU_usage")
+        metric = Metric("host_pid_cpu_usage")
         metric.with_timestamp(datetime.timestamp(now))
         metric.add_tag('host', socket.gethostname())
         metric.add_tag('platform', platform.platform())
@@ -37,8 +37,6 @@ def setSeverity(cpu_percent, pname, pID):
         Compares system CPU times elapsed before and after interval. Should be at least .1
         Pass in CPU_Percent to compare against and the Process's name to attach to summary.
         '''
-
-
         if cpu_percent >= args.warning and cpu_percent < args.critical:
                 return 'WARNING', 'CPU utilization at %s percent on %s %s'%(cpu_percent, pname, pID)
                 #sys.exit(1)
@@ -49,79 +47,53 @@ def setSeverity(cpu_percent, pname, pID):
                 return 'OK', 'CPU utilization at %s percent on %s %s'%(cpu_percent, pname, pID)
                 #sys.exit(0)
 
+def hoglist(delay=5):
+    '''Return a list of processes using a nonzero CPU percentage
+       during the interval specified by delay (seconds),
+       sorted so the biggest hog is first.
+    '''
+    proccesses = list(psutil.process_iter())
+    for proc in proccesses:
+        proc.cpu_percent(None)    # non-blocking; throw away first bogus value
 
-def GetProcessInformation(p):
-        '''
-        Passes in a PID to then returns process information as dict.
-        Information includes: PID, Component(name), User,
-        CPU_Usage(interval=1), Severity, Summary, Timestamp,
-        Source, Class, and Group
-        '''
-        # Will skip over the idle process which will always be PID 0
-        if(p == 0): return
+    sys.stdout.flush()
+    time.sleep(delay)
 
-        CPU_INTERVAL = 1
-        proc = {}
+    procs = []
+    for proc in proccesses:
+        # Skip system idle process which is always pid 0.
+        # System Idle process will always be the 'highest' cpu hog
+        if (proc.pid == 0): continue
+        # Skip python when running the program
+        # if (proc.name() == 'python'): continue
         try:
-                p = psutil.Process(p)
-                # Sets the proc to a dictionary with the Process information
-                # pid, name, & username are attributes that are attached to a process
-                proc['PID'], proc['Process'], proc['User'] = p.pid, p.name(), p.username()
-
-                # Creates a reference point for the process's CPU checks
-                #p.cpu_percent(interval=CPU_INTERVAL)
-
-                # Does the second CPU check and records it to the dictionary
-                proc['CPU_Usage'] = p.cpu_percent(interval=CPU_INTERVAL)
-                
-                # Checks and records the severity and summary with the setSeverity()
-                proc['Severity'], proc['Summary'] = setSeverity(proc['CPU_Usage'], proc['Process'], proc['PID'])
-                
-                # Gets current timestamp, hostname & IP
-                proc['Timestamp'] = datetime.utcnow().isoformat() + 'Z'
-                proc['Source'] = socket.gethostname()
-                
-                # This is where you cna set the class and Group for PagerDuty integration
-                proc['Class'] = 'Process by CPU'
-                proc['Group'] = ''
-                
+                percent = proc.cpu_percent(None)
+                severity, summary = setSeverity(percent, proc.name(), proc.pid)
+                if percent:
+                        procs.append({
+                            'pid': proc.pid,
+                            'name': proc.name(),
+                            'user': proc.username(),
+                            'cpu_percent': percent,
+                            'severity': severity,
+                            'summary': summary,
+                            'source': socket.gethostname(),
+                            'class': 'Process by CPU',
+                            # 'timestamp': datetime.utcnow().isoformat() + 'Z',          
+                            # 'group': ''            
+                            })
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
-
-        # Ensures that proc is not returned unless CPU_Usage is over 0
-        # Returning usable data 
-        if (bool(proc) and proc['CPU_Usage'] > 0.0 ):
-                return proc
+    
+    return procs
 
 def main():
-        # splits up the lookup and check of each process.
-        # mp is for multiprocessing - pool will split based on how many processors there are
-        pool = mp.Pool(mp.cpu_count())
-        
-        # pool.map will run the function GetProcessInformation on every item in psutil.pids.
-        # List(filter()) will remove anything that returns 
-        pid_list = list(filter(None, pool.map(GetProcessInformation, psutil.pids())))
-        
-        # Sorts pid_list by CPU_usage
-        pid_list = sorted(pid_list, key = lambda i: i['CPU_Usage'],reverse=True)
+        pid_list = hoglist()
+        pid_list.sort(key = lambda i: i['cpu_percent'], reverse=True)
 
-        # To avoid a potential index error run loop through a range of the pid_list length.
-        # Breaks out of loop after 5 prints, i.e top five processes.
-        for x in range(len(pid_list)):
-                if (args.influxout):
-                        influxOutput(pid_list[x])
-                else:
-                        # Prints the summary, source, severity, timestamp, procname with PID, and Group(blank)
-                        print(pid_list[x]['Summary'], pid_list[x]['Source'], pid_list[x]['Severity'], pid_list[x]['Timestamp'], pid_list[x]['Class'], pid_list[x]['Process'] + ' - ' + str(pid_list[x]['PID']), pid_list[x]['Group'], sep='\t')
-                
-                if (x == 4): break
-        # If statement occurs when pid_list does not have at least 5 entries, this allows for 
-        # 5 print outs to keep the top5 format
-        if (len(pid_list) < 5):
-                x = len(pid_list)
-                while(x < 5):
-                        print('-', '-', '-', '-', '-', '-', sep='\t')
-                        x += 1
+        for x in range(5):
+                influxOutput(pid_list[x]) if args.influxout else print(pid_list[x])
+
         sys.exit(0)
 
 if __name__ == '__main__':
